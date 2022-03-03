38948103799fd814e4c708f0050ac35b2b0d68be
iktp <hexianke2020@gmail.com>
2022-03-03 10:44:23 +0100

    original code

---
+++ include/Shape.h
@@ -0,0 +1,32 @@
+#ifndef SHAPE1_H
+#define SHAPE1_H
+
+class Shape
+{
+public:
+	Shape();
+	~Shape();
+
+        virtual float CalculateArea(){};
+        virtual float CalculateCircumference(){};
+
+};
+
+
+class Rectangle: public Shape{
+
+ public:
+    	Rectangle(float,float);
+        ~Rectangle();
+
+       float CalculateArea();
+       float CalculateCircumference();
+
+ public:
+    	  float Length;
+          float Width;
+
+};
+
+
+#endif
