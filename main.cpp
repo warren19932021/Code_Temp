d9c3a5297a3d53592e4f9d17f0ff06b547b6cb53
Xianke he <hexianke2020@gmail.com>
2022-03-23 15:21:44 +0100

    a second test step to merge worker B

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 
       Shape * shape_A = new Square(10);
+
 		cout<<"Area of Square: "<<shape_A->CalculateArea()<<endl;
 		cout<<"circumference of Square: "<<shape_A->CalculateCircumference()<<endl;
  
